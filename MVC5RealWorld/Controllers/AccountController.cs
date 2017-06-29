6bb35c486bf7bac50694224fd16fcb88bfa91b82
Hien Minh <nguyenminhhienit@gmail.com>
2017-06-30 06:19:02 +0700

    Initial
    Initial

---
+++ MVC5RealWorld/Controllers/AccountController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5RealWorld.Models.EntityManager;
+using MVC5RealWorld.Models.ViewModel;
+using System.Web.Security;
+
+namespace MVC5RealWorld.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(UserSignUpView USV)
+        {
+            if (ModelState.IsValid)
+            {
+                UserManager UM = new UserManager();
+                if (!UM.IsLoginNameExist(USV.LoginName))
+                {
+                    UM.AddUserAccount(USV);
+                    FormsAuthentication.SetAuthCookie(USV.FirstName, false);
+                    return RedirectToAction("Welcome", "Home");
+                }
+                else
+                    ModelState.AddModelError("", "Login name already taken");
+            }
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(UserLoginView ULV)
+        {
+            if (ModelState.IsValid)
+            {
+                UserManager UM = new UserManager();
+                var password = UM.GetUserPassword(ULV.LoginName);
+                if (string.IsNullOrEmpty(password))
+                {
+                    ModelState.AddModelError("", "The user login or password provided is incorrect.");
+                }
+                else
+                {
+                    if (ULV.Password.Equals(password))
+                    {
+                        FormsAuthentication.SetAuthCookie(ULV.LoginName, false);
+                        return RedirectToAction("Welcome", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "The password provided is incorrect.");
+                    }
+                }
+            }
+            return View(ULV);
+        }
+        [Authorize]
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }       
+    }
+}
